6e1b20f73332abf29b81f28aa7c38e4dde5e3624
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-03-05 10:44:18 +0100

    push

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const itemEl = document.querySelectorAll(`.item`);
+
+console.log(`Number of categories: ${itemEl.length}`);
+
+itemEl.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.innerHTML}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
+onkeydown;
