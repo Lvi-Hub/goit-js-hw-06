737b393c536c0c75f91579d14b0c198a13df06da
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-03-06 21:51:20 +0100

    task_5

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,10 @@
 const inputFieldEl = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-const inputText = inputFieldEl.addEventListener(inputFieldEl, (event) => {
-  output.textContent = event.currentTarget.value;
+const inputText = inputFieldEl.addEventListener("input", (event) => {
+  event.currentTarget.value === ""
+    ? (output.textContent = "Anonymous")
+    : (output.textContent = event.currentTarget.value);
 });
 
-console.log(output);
-
-const textInput = document.querySelector(".text-input");
-const output = document.querySelector(".output");
-
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-});
+console.log(inputFieldEl);
