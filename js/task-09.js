9beac7b34d3df95f9abafd1fd063e08e42813260
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-03-07 21:35:34 +0100

    Task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const buttonEl = document.querySelector(".change-color");
+const bodyStyleEl = document.querySelector("body");
+const textEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", (event) => {
+  const changeColor = getRandomHexColor();
+  bodyStyleEl.style.color = changeColor;
+  textEl.textContent = changeColor;
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
