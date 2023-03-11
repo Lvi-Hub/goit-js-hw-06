c7a05d83ea15dae44180e3cfbe30760abb8f4d82
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-03-11 22:41:51 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const bodyStyleEl = document.querySelector("body");
+const divEl = document.createElement("div");
+bodyStyleEl.append(divEl);
+
+// let array = [];
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// function createBoxes(amount) {
+//   for (let i = 0; i < 5; i += 1) {
+//     const div = `<div> ${i} style:"width = 30px, height = 30px" </div>`;
+//     array.push(div);
+//   }
+// }
+// boxesEl.append(...array);
+// console.log(createBoxes());
+// console.log(array.join(""));
+// //boxesEl.append(array.join(""));
